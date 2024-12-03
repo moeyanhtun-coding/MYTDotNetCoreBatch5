800a133c09f1782e9ead15a447956eb4166b7e32
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-03 06:46:05 +0630

    feat: add AppDbContext for EFcore

---
+++ MYTDotNetCore.ConsoleApp/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MYTDotNetCore.ConsoleApp
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer();
+            }
+        }
+    }
+}
+
