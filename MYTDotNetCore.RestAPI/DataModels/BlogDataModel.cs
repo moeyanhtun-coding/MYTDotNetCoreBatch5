13cb259e6e363539015eeea7d3af49590e43ae63
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-08 12:23:51 +0630

    feat: create BlogDataModel

---
+++ MYTDotNetCore.RestAPI/DataModels/BlogDataModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MYTDotNetCore.RestAPI.DataModels
+{
+    public class BlogDataModel
+    {
+        public int Id { get; set; }
+        public string? BlogTitle { get; set; }
+        public string? BlogAuthor { get; set; }
+        public string? BlogContent { get; set; }
+        public bool DeleteFlag { get; set; }
+    }
+}
