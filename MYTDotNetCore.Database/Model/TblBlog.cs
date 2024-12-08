05101c0de261bafde96359879e7ea3d80954b559
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-08 11:42:27 +0630

    feat: nullable for model

--- MYTDotNetCore.Database/Model/TblBlog.cs
+++ MYTDotNetCore.Database/Model/TblBlog.cs
@@ -7,11 +7,11 @@ public partial class TblBlog
 {
     public int BlogId { get; set; }
 
-    public string BlogTitle { get; set; } = null!;
+    public  string? BlogTitle { get; set; } = null!;
 
-    public string BlogAuthor { get; set; } = null!;
+    public string? BlogAuthor { get; set; } = null!;
 
-    public string BlogContent { get; set; } = null!;
+    public string? BlogContent { get; set; } = null!;
 
-    public bool DeleteFlag { get; set; }
+    public bool? DeleteFlag { get; set; }
 }
