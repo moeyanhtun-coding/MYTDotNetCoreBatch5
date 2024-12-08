6fcb0d0e6fed90814e077de3f6615918b5a999da
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-08 10:28:13 +0630

    feat: Blog RESTful API for Efcore CRUD

--- MYTDotNetCore.RestAPI/Controllers/BlogsController.cs
+++ MYTDotNetCore.RestAPI/Controllers/BlogsController.cs
@@ -1,28 +1,56 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MYTDotNetCore.Database.Model;
 
 namespace MYTDotNetCore.RestAPI.Controllers
 {
+   
     [Route("api/[controller]")]
     [ApiController]
     public class BlogsController : ControllerBase
     {
+        private readonly AppDbContext _db = new AppDbContext();
         [HttpGet]
         public IActionResult GetBlogs()
         {
-            return Ok();
+            var lst = _db.TblBlogs.AsNoTracking()
+                .Where(x => x.DeleteFlag != true)
+                .ToList();
+            return Ok( new { data = lst});
         }
 
         [HttpPost]
-        public IActionResult CreateBlog()
+        public IActionResult CreateBlog(TblBlog blog)
         {
-            return Ok();
+            _db.TblBlogs.Add(blog);
+            _db.SaveChanges();
+            return Ok(blog);
         }
 
-        [HttpPut]
-        public IActionResult UpdateBlog()
+        [HttpGet("{id}")]
+        public IActionResult EditBlog(int id)
         {
-            return Ok();
+            var item = _db.TblBlogs.AsNoTracking().
+                FirstOrDefault(x => x.DeleteFlag != true && x.BlogId == id);
+            return Ok(item);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateBlog(int id, TblBlog blog)
+        {
+            var item = _db.TblBlogs.AsNoTracking()
+                .FirstOrDefault(x => x.DeleteFlag != true && x.BlogId == id);
+            if(item is null)
+            {
+                return NotFound();
+            }
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent;
+
+            _db.Entry(item).State = EntityState.Modified;
+            _db.SaveChanges();
+            return Ok(item);
         }
 
         [HttpPatch]
