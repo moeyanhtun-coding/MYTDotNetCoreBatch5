892af9d98d36950255be8bbf4cd18656b447583c
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-12 09:21:28 +0630

    feat: code clean

--- MYTDotNetCore.ConsoleApp/AdoDotNetExample2.cs
+++ MYTDotNetCore.ConsoleApp/AdoDotNetExample2.cs
@@ -1,11 +1,5 @@
 ﻿using MYTDotNetCore.Shared;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static MYTDotNetCore.Shared.AdoDotNetService;
 
 namespace MYTDotNetCore.ConsoleApp
