541b5321f296b658457dcde9796d88b6d1a1bc6b
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-12 09:21:49 +0630

    feat: create DapperServices

---
+++ MYTDotNetCore.Shared/DapperService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace MYTDotNetCore.Shared
+{
+    public class DapperService
+    {
+        private readonly string _connectionString;
+        public DapperService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<T> Query<T>(string query, object? param = null)
+        {
+            using IDbConnection db = new SqlConnection(_connectionString);
+            var lst = db.Query<T>(query, param).ToList();
+            return lst;
+        }
+
+        public T QueryFirstOrDefault<T>(string query, object? param = null)
+        {
+            using IDbConnection db = new SqlConnection(_connectionString);
+            var item = db.Query<T>(query, param).FirstOrDefault()!;
+            return item;
+        }
+
+        public string Execute(string query, object? param = null)
+        {
+            using IDbConnection db = new SqlConnection(_connectionString);
+            int result = db.Execute(query, param);
+            return result > 0 ? "Operation Successful" : "Operation Fail";
+        }
+
+    }
+}
