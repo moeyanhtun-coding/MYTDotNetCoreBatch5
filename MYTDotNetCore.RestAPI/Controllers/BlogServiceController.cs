3fec2af23e51bbe0cc07b3ac71f7890f9ede5f71
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-21 23:27:47 +0630

    feat : apply BaseResponseModel Feature in Create Blog

--- MYTDotNetCore.RestAPI/Controllers/BlogServiceController.cs
+++ MYTDotNetCore.RestAPI/Controllers/BlogServiceController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MYTDotNetCore.Database.Model;
 using MYTDotNetCore.Domain.Feature.Blog;
+using MYTDotNetCore.Domain.Model;
 
 namespace MYTDotNetCore.RestAPI.Controllers;
 
@@ -10,6 +11,7 @@ namespace MYTDotNetCore.RestAPI.Controllers;
 public class BlogServiceController : ControllerBase
 {
     private readonly BlogService _blogService = new BlogService();
+
     [HttpGet]
     public IActionResult GetBlogs()
     {
@@ -27,18 +29,30 @@ public class BlogServiceController : ControllerBase
     }
 
     [HttpPost]
-    public async Task< IActionResult> CreateBlog(TblBlog reqModel)
+    public async Task<IActionResult> CreateBlog(TblBlog reqModel)
     {
-        var result = await _blogService.CreateBlog(reqModel);
-        return Ok(result);
+        try
+        {
+            var model = await _blogService.CreateBlog(reqModel);
+            if (model.Response.RespType == EnumRespType.ValidationError)
+                return BadRequest(model);
+            if (model.Response.RespType == EnumRespType.SystemError)
+                return StatusCode(StatusCodes.Status500InternalServerError, model);
+
+            return Ok(model);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(new { message = e.Message });
+        }
     }
 
     [HttpPatch("{id}")]
-    public IActionResult PatchBlog( int id, TblBlog reqModel)
+    public IActionResult PatchBlog(int id, TblBlog reqModel)
     {
         var result = _blogService.UpdateBlog(id, reqModel);
         if (result is 0) return BadRequest(new { message = "Blog Updated Fail" });
-        return Ok(new { message = "Blog Update Successful"});
+        return Ok(new { message = "Blog Update Successful" });
     }
 
     [HttpDelete]
