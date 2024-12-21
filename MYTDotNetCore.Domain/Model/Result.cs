d72ae869501c29673416a0a9654f79797c1b25e8
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-21 23:45:58 +0630

    feat : create Result Model

---
+++ MYTDotNetCore.Domain/Model/Result.cs
@@ -0,0 +1,9 @@
+﻿namespace MYTDotNetCore.Domain.Model;
+
+public class Result<T>
+{
+    public bool IsSuccess { get; set; }
+    public bool IsError { get { return !IsSuccess; } }
+    public EnumRespType Type { get; set; }
+    public T Data { get; set; }
+}
