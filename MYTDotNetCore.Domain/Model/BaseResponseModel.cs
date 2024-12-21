3eeb1b5a982ca54d0427ac839410ff682d263c11
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-21 11:01:17 +0630

    feat : BaseResponseModel Create

---
+++ MYTDotNetCore.Domain/Model/BaseResponseModel.cs
@@ -0,0 +1,55 @@
+﻿namespace MYTDotNetCore.Domain.Model;
+
+public class BaseResponseModel
+{
+    public string RespCode { get; set; }
+    public string RespDesc { get; set; }
+    public EnumRespType RespType { get; set; }
+    public bool IsSuccess { get; set; }
+
+    public bool IsError
+    {
+        get { return !IsSuccess; }
+    }
+
+    public static BaseResponseModel Success(string respCode, string respDesc)
+    {
+        return new BaseResponseModel
+        {
+            RespCode = respCode,
+            RespDesc = respDesc,
+            IsSuccess = true,
+            RespType = EnumRespType.Success
+        };
+    }
+
+    public static BaseResponseModel ValidationError(string respCode, string respDesc)
+    {
+        return new BaseResponseModel
+        {
+            RespCode = respCode,
+            RespDesc = respDesc,
+            IsSuccess = false,
+            RespType = EnumRespType.ValidationError
+        };
+    }
+
+    public static BaseResponseModel SystemError(string respCode, string respDesc)
+    {
+        return new BaseResponseModel
+        {
+            RespCode = respCode,
+            RespDesc = respDesc,
+            IsSuccess = false,
+            RespType = EnumRespType.SystemError
+        };
+    }
+}
+
+public enum EnumRespType
+{
+    None,
+    Success,
+    ValidationError,
+    SystemError,
+}
