8fdae97e1bc2a63fb4279f30e01e0c10724ad1fd
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-21 23:27:15 +0630

    feat : BaseController Create

---
+++ MYTDotNetCore.RestAPI/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MYTDotNetCore.Domain.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MYTDotNetCore.RestAPI;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : Controller
+{
+    public IActionResult Execute(Object model)
+    {
+        JObject jObj = JObject.Parse(JsonConvert.SerializeObject(model));
+        if (jObj.ContainsKey("Response"))
+        {
+            BaseResponseModel baseResponseModel =
+                JsonConvert.DeserializeObject<BaseResponseModel>(jObj["Response"]!.ToString())!;
+            if(baseResponseModel.RespType == EnumRespType.ValidationError)
+                return BadRequest(model);
+            if(baseResponseModel.RespType == EnumRespType.SystemError)
+                return StatusCode(500, model);
+            return Ok(model); 
+        }
+        return StatusCode(500, "Internal Response Model Error. Please add BaseResponseModel to your ResponseModel");
+    }
+}
