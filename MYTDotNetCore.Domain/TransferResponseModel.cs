70c22dff922320c78e1614ea1f423f3618040096
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-22 23:19:00 +0630

    feat : add ResultBlogResponsemodel

--- MYTDotNetCore.Domain/TransferResponseModel.cs
+++ MYTDotNetCore.Domain/TransferResponseModel.cs
@@ -1,8 +1,14 @@
-﻿using MYTDotNetCore.Domain.Model;
+﻿using MYTDotNetCore.Database.Model;
+using MYTDotNetCore.Domain.Model;
 
 namespace MYTDotNetCore.Domain;
 
 public class TransferResponseModel
 {
     public BaseResponseModel Response { get; set; }
+}
+
+public class ResultBlogResponseModel
+{
+    public TblBlog Blog { get; set; } 
 }
