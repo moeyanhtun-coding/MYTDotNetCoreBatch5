2b65783c14c28007b76af51c78479a67cae4ed08
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-22 23:18:34 +0630

    feat : Add result model

--- MYTDotNetCore.Domain/Model/Result.cs
+++ MYTDotNetCore.Domain/Model/Result.cs
@@ -3,7 +3,24 @@
 public class Result<T>
 {
     public bool IsSuccess { get; set; }
-    public bool IsError { get { return !IsSuccess; } }
+
+    public bool IsError
+    {
+        get { return !IsSuccess; }
+    }
+
     public EnumRespType Type { get; set; }
     public T Data { get; set; }
+
+    public static Result<T> Success<T>(T data, string message = "Success")
+    {
+        return new Result<T>
+        {
+            IsSuccess = true,
+            Type = EnumRespType.Success,
+            Data = data,
+        };
+    }
+    
+
 }
