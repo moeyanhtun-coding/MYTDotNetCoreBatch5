051d2ccda9796a5ad60a36d9464e9aacb9c729dd
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-24 22:22:56 +0630

    feat : add ValidationError For Response and SystemError Response

--- MYTDotNetCore.Domain/Model/Result.cs
+++ MYTDotNetCore.Domain/Model/Result.cs
@@ -1,4 +1,6 @@
-﻿namespace MYTDotNetCore.Domain.Model;
+﻿using Azure.Core;
+
+namespace MYTDotNetCore.Domain.Model;
 
 public class Result<T>
 {
@@ -9,18 +11,50 @@ public class Result<T>
         get { return !IsSuccess; }
     }
 
-    public EnumRespType Type { get; set; }
+    public bool IsValidationError
+    {
+        get { return Type == EnumRespType.ValidationError; }
+    }
+
+    public bool IsSystemError
+    {
+        get { return Type == EnumRespType.SystemError; }
+    }
+
+    private EnumRespType Type { get; set; }
+    public string Message { get; set; }
     public T Data { get; set; }
 
-    public static Result<T> Success<T>(T data, string message = "Success")
+    public static Result<T> Success(T data, string message = "Success")
     {
         return new Result<T>
         {
             IsSuccess = true,
             Type = EnumRespType.Success,
             Data = data,
+            Message = message
         };
     }
-    
 
+    public static Result<T> ValidationError(string message, T? data = default)
+    {
+        return new Result<T>
+        {
+            IsSuccess = false,
+            Type = EnumRespType.ValidationError,
+            Message = message,
+            Data = data
+        };
+    }
+
+    public static Result<T> SystemError(string message, T? data = default)
+    {
+        return new Result<T>
+        {
+            IsSuccess = false,
+            Type = EnumRespType.SystemError,
+            Message = message,
+            Data = data
+        };
+    }
 }
