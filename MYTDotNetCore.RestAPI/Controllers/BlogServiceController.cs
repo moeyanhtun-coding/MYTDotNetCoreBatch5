b071cf9e8b91286851cea291b5feaecb6b32db0a
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-25 09:45:52 +0630

    -

--- MYTDotNetCore.RestAPI/Controllers/BlogServiceController.cs
+++ MYTDotNetCore.RestAPI/Controllers/BlogServiceController.cs
@@ -5,71 +5,72 @@ using MYTDotNetCore.Domain;
 using MYTDotNetCore.Domain.Feature.Blog;
 using MYTDotNetCore.Domain.Model;
 
-namespace MYTDotNetCore.RestAPI.Controllers;
-
-[Route("api/[controller]")]
-[ApiController]
-public class BlogServiceController : BaseController
+namespace MYTDotNetCore.RestAPI.Controllers
 {
-    private readonly BlogService _blogService = new BlogService();
-
-    [HttpGet]
-    public IActionResult GetBlogs()
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogServiceController : BaseController
     {
-        var lst = _blogService.GetBlogs();
-        return Ok(lst);
-    }
+        private readonly BlogService _blogService = new BlogService();
 
-    [HttpGet("{id}")]
-    public IActionResult GetBlogs(int id)
-    {
-        var item = _blogService.GetBlogs(id);
-        if (item is null)
-            return BadRequest(new { message = "Item not found" });
-        return Ok(item);
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> CreateBlogResult(TblBlog blog)
-    {
-        try
+        [HttpGet]
+        public IActionResult GetBlogs()
         {
-            var model = await _blogService.CreateBlogResult(blog);
-            return Execute(model);
+            var lst = _blogService.GetBlogs();
+            return Ok(lst);
         }
-        catch (Exception ex)
-        {
-            return BadRequest(new { message = ex.Message });
-        }
-    }
 
-    [HttpPost]
-    public async Task<IActionResult> CreateBlog(TblBlog reqModel)
-    {
-        try
+        [HttpGet("{id}")]
+        public IActionResult GetBlogs(int id)
         {
-            var model = await _blogService.CreateBlog(reqModel);
-            return Execute(model);
+            var item = _blogService.GetBlogs(id);
+            if (item is null)
+                return BadRequest(new { message = "Item not found" });
+            return Ok(item);
         }
-        catch (Exception e)
+
+        [HttpPost("create-blog-result")]
+        public async Task<IActionResult> CreateBlogResult(TblBlog blog)
         {
-            return BadRequest(new { message = e.Message });
+            try
+            {
+                var model = await _blogService.CreateBlogResult(blog);
+                return Execute(model);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
         }
-    }
+        
+        [HttpPost("create-blog")]
+        public async Task<IActionResult> CreateBlog(TblBlog reqModel)
+        {
+            try
+            {
+                var model = await _blogService.CreateBlog(reqModel);
+                return Execute(model);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { message = e.Message });
+            }
+        }
 
-    [HttpPatch("{id}")]
-    public IActionResult PatchBlog(int id, TblBlog reqModel)
-    {
-        var result = _blogService.UpdateBlog(id, reqModel);
-        if (result is 0) return BadRequest(new { message = "Blog Updated Fail" });
-        return Ok(new { message = "Blog Update Successful" });
-    }
+        [HttpPatch("{id}")]
+        public IActionResult PatchBlog(int id, TblBlog reqModel)
+        {
+            var result = _blogService.UpdateBlog(id, reqModel);
+            if (result is 0) return BadRequest(new { message = "Blog Updated Fail" });
+            return Ok(new { message = "Blog Update Successful" });
+        }
 
-    [HttpDelete]
-    public IActionResult DeleteBlog(int id)
-    {
-        var result = (_blogService.DeleteBlog(id));
-        if (result is 0) return BadRequest(new { message = "Blog Delete Fail" });
-        return Ok(new { message = "Blog Delete Successful" });
+        [HttpDelete]
+        public IActionResult DeleteBlog(int id)
+        {
+            var result = (_blogService.DeleteBlog(id));
+            if (result is 0) return BadRequest(new { message = "Blog Delete Fail" });
+            return Ok(new { message = "Blog Delete Successful" });
+        }
     }
 }
