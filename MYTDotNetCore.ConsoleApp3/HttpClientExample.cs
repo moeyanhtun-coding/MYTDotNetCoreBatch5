5f5c4e08ac588ffdad8daf710aff4289d8c39c27
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-28 15:29:06 +0630

    faet : Read and readById

---
+++ MYTDotNetCore.ConsoleApp3/HttpClientExample.cs
@@ -0,0 +1,32 @@
+﻿namespace MYTDotNetCore.ConsoleApp3;
+
+public class HttpClientExample
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _endPoint = "https://localhost:7274/api/Blogs";
+
+    public HttpClientExample()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public async Task ReadAsync()
+    {
+        var response = await _httpClient.GetAsync (_endPoint);
+        if (response.IsSuccessStatusCode)
+        {
+            var jsonStr = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(jsonStr);
+        }
+    }
+
+    public async Task GetByIdAsync(int id)
+    {
+        var response = await _httpClient.GetAsync(_endPoint + "/" + id);
+        if (response.IsSuccessStatusCode)
+        {
+            var jsonStr = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(jsonStr);
+        }
+    }
+}
