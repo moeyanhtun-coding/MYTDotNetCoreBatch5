9978a6605a0af05d38210d9bf5ab2776f0b769fb
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-28 11:21:29 +0630

    -

---
+++ MYTDotNetCore.ConsoleApp3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+HttpClient http = new HttpClient();
+
+var response =  await http.GetAsync("https://jsonplaceholder.typicode.com/posts/55");
+if (response.IsSuccessStatusCode)
+{
+    string jsonStr = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(jsonStr);
+}
