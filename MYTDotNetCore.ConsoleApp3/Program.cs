b8b05bb971cece5ad7e8e402b42a7e90698fe9a6
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-30 18:30:19 +0630

    feat : RestClient Example

--- MYTDotNetCore.ConsoleApp3/Program.cs
+++ MYTDotNetCore.ConsoleApp3/Program.cs
@@ -4,8 +4,13 @@ using MYTDotNetCore.ConsoleApp3;
 
 Console.WriteLine("Hello, World!");
 
-HttpClientExample httpClientExample = new HttpClientExample();
+// HttpClientExample httpClientExample = new HttpClientExample();
+//
+// await httpClientExample.Edit(21);
+// await httpClientExample.Patch(22, "honey Update", "","" );
+// await httpClientExample.Edit(22);
 
-await httpClientExample.Edit(21);
-await httpClientExample.Patch(22, "honey Update", "","" );
-await httpClientExample.Edit(22);
+
+RestClientExample _client = new RestClientExample();
+
+await _client.GetBlogs();
