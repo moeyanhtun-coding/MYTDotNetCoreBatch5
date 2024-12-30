35ce40d0dbe379e31e8b29b63826899f4caefc29
Moe Yan Htun <moeyan227970@gmail.com>
2024-12-30 21:03:21 +0630

    feat : refit client List Blogs and blog

---
+++ MYTDotNetCore.ConsoleApp3/IBlogApi.cs
@@ -0,0 +1,22 @@
+﻿using Refit;
+
+namespace MYTDotNetCore.ConsoleApp3;
+
+public interface IBlogApi
+{
+    [Get("/api/blogs")]
+    Task<List<BlogModel>> GetBlogs();
+    
+    [Get("/api/blogs/{id}")]
+    Task<BlogModel> GetBlogById(int id);
+    
+}
+
+public class BlogModel
+{
+    public int Id { get; set; }
+    public string? BlogTitle { get; set; }
+    public string? BlogAuthor { get; set; }
+    public string? BlogContent { get; set; }
+    public bool DeleteFlag { get; set; }
+}
