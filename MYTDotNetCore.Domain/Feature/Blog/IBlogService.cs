cc8aa57a208c1f8a31932cee3313d91845221c02
Moe Yan Htun <moeyan227970@gmail.com>
2025-01-03 22:03:00 +0630

    feat : Create IBlogService.cs

---
+++ MYTDotNetCore.Domain/Feature/Blog/IBlogService.cs
@@ -0,0 +1,14 @@
+﻿using MYTDotNetCore.Database.Model;
+using MYTDotNetCore.Domain.Model;
+
+namespace MYTDotNetCore.Domain.Feature.Blog;
+
+public interface IBlogService
+{
+    List<TblBlog> GetBlogs();
+    TblBlog GetBlogs(int id);
+    Task<Result<ResultBlogResponseModel>> CreateBlogResult(TblBlog reqModel);
+    Task<BlogResponseModel> CreateBlog(TblBlog reqModel);
+    int UpdateBlog(int id, TblBlog reqModel);
+    int DeleteBlog(int id);
+}
