d3bee7e3c6ddfe188711c07d5348f9b049798ea7
Moe Yan Htun <moeyan227970@gmail.com>
2025-01-14 22:15:28 +0630

    -

--- MYTDotNetCore.Domain/Feature/Blog/BlogService.cs
+++ MYTDotNetCore.Domain/Feature/Blog/BlogService.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using MYTDotNetCore.Database.Model;
 using MYTDotNetCore.Domain.Model;
 
 namespace MYTDotNetCore.Domain.Feature.Blog;
